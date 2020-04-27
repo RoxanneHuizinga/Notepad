a099ec807680a64f5a2bc57df96b5316eb6485de
Roxy-Hva <roxy.huizinga@hva.nl>
2020-04-27 10:25:36 +0200

    changes

--- app/src/androidTest/java/com/example/notepad/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/notepad/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.slepnev.notepadkotlin", appContext.packageName)
+        assertEquals("com.example.notepadkotlin", appContext.packageName)
     }
 }
